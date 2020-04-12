38ece07c20b070ab2943a25bd0b42ec366b1cc23
AAKON <FAISALAKON444@GMAIL.COM>
2020-04-12 15:17:11 +0600

    District wise corona cases data scraping

---
+++ districtWiseDataScraping.py
@@ -0,0 +1,41 @@
+import tabula
+import pandas as pd
+import re
+import requests
+
+url = 'https://www.iedcr.gov.bd/website/images/files/nCoV/Confirmed%20cases%20in%20Bangladesh_upto%20April%2011_2020.pdf'
+myfile = requests.get(url)
+open('iedcr.pdf', 'wb').write(myfile.content)
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+df = tabula.read_pdf("iedcr.pdf", encoding='utf-8')
+
+for x in df:
+    s=str(x)
+
+#df.to_csv('output.csv', encoding='utf-8')
+#print(file)
+s = re.sub(' +',',',s)
+s = re.sub('NaN+',',',s)
+s = re.sub(',+',' ',s)
+s = StringIO(s)
+with open('DistrictWiseCase.csv', 'w') as f:
+    f.write('0,City,case\n')
+    for line in s:
+        res = len(line.split())
+        if res<5:
+            if res==4:
+                string = line.split(" ")
+                string.pop(1)
+                line=' '.join(string)
+            line = re.sub(' +',',',line)
+            f.write(line)
+            
+        
+
+print("Two file created,DistrictWiseCase.csv & iedcr.pdf")
+
+print('finished')
+
