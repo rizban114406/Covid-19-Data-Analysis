a2e916fc85a53cbcbee5d0f631748c4b03d39baf
AAKON <FAISALAKON444@GMAIL.COM>
2020-04-14 11:52:46 +0600

    removing left side defult title 0

--- Analysis Code/CoronaTestSummaryBD.py
+++ Analysis Code/CoronaTestSummaryBD.py
@@ -4,20 +4,36 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import matplotlib.dates as mdates
 from matplotlib import style
+
 def func(pct, allvals):
     absolute = int(pct/100.*np.sum(allvals))
     return "{:.1f}%\n({:d})".format(pct, absolute)
+
+print(plt.style.available)
+##style.use('fivethirtyeight')
+fig = plt.figure()
+ax1 = plt.subplot2grid((1,1), (0,0))
 CoronaTestCaseNo = pd.read_csv('../Analysis Data/CoronaTestCaseNo.csv')
-df = pd.Series([int(CoronaTestCaseNo.iloc[4,4]),int(CoronaTestCaseNo.iloc[2,4])])
-df.index=['Positive','Test_Conducted']
 
-slices=int(CoronaTestCaseNo.iloc[4,4])+int(CoronaTestCaseNo.iloc[2,4])
-my_explode=(0,0.2)
 
-df.plot.pie(subplots=True,figsize=(10, 10),shadow= True,
-            title="OVERALL -- Positive vs Test_Conducted",
-            colors=['#8B0000','#CD5C5C'],
-            explode=my_explode, startangle=90,
-            autopct=lambda pct: func(pct, slices),
-            labels=['OVERALL -- Positive vs Test_Conducted','OVERALL -- Positive vs Test_Conducted'])
+Positive = int(CoronaTestCaseNo.iloc[4,4])
+Test_Conducted = int(CoronaTestCaseNo.iloc[2,4])
+
+
+slices = [Positive,Test_Conducted]
+activities = ['Positive','Test_Conducted']
+cols = ['#CD5C5C','#8B0000']
+ax1.pie(slices,
+        labels=activities,
+        colors=cols,
+        startangle=90, #Starting angle point
+        shadow= True,
+        explode=(0.1,.1), #Slice out (amount of explotion sleeping, ammount of eating,..)
+##        autopct='%1.1f%%'
+        autopct=lambda pct: func(pct, slices)) # Adds the percentage
+
+#for i in ax1.patches:
+#    print(i)
+
+plt.title('OVERALL -- Positive vs Test_Conducted')
 plt.show()
